3f98571ecbb5944003eee54b4e57ffdcf1ecafef
Elics <48738735+Elics@users.noreply.github.com>
2019-11-18 18:52:15 -0500

    Add files via upload

---
+++ Alpha.java
@@ -0,0 +1,65 @@
+// alphabetical order of 3 words
+import javax.swing.JOptionPane;
+
+public class Alpha
+{
+   public static void main(String[] args)
+   {
+      /* String name1, name2, name3;
+      name1 = "Charlie";
+      name2 = "Leslie";
+      name3 = "Andy";
+      */
+      String name1, name2, name3;
+      String output  ="";
+      
+      
+      name1   = JOptionPane.showInputDialog("Enter the first book title: ");
+      name2   = JOptionPane.showInputDialog("Enter the second book title: ");
+      name3   = JOptionPane.showInputDialog("Enter the third book title: ");
+      
+      if (name1.compareToIgnoreCase(name2) <0 && name1.compareToIgnoreCase(name3)<0)
+      { output  +=name1+ "\n";
+         if (name2.compareToIgnoreCase(name3) <0){
+         output +=name2 +"\n";
+         output +=name3 + "\n";
+         }
+         else{
+         output +=name3 +"\n";
+         output +=name2 +"\n";
+         }
+     }
+     
+       else if (name2.compareToIgnoreCase(name1) <0 && name2.compareToIgnoreCase(name3)<0)
+      { output  +=name2+ "\n";
+         if (name1.compareToIgnoreCase(name3) <0){
+         output +=name1 +"\n";
+         output +=name3 + "\n";
+         }
+         else{
+         output +=name3 +"\n";
+         output +=name1 +"\n";
+         }
+     }
+
+  else if (name3.compareToIgnoreCase(name2) <0 && name3.compareToIgnoreCase(name1)<0)
+      { output  +=name3+ "\n";
+         if (name2.compareToIgnoreCase(name1) <0){
+         output +=name2 +"\n";
+         output +=name1 + "\n";
+         }
+         else{
+         output +=name1 +"\n";
+         output +=name2 +"\n";
+         }
+     }
+
+  
+   
+   JOptionPane.showMessageDialog(null, output);
+   
+         
+     System.exit(0);    
+  
+   }
+}
