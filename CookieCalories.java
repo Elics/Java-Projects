ec1798562adf7a1c42bd8084c5b29dd7e2ba8563
Elics <48738735+Elics@users.noreply.github.com>
2019-11-18 18:47:38 -0500

    Update CookieCalories.java

--- CookieCalories.java
+++ CookieCalories.java
@@ -1,3 +1,14 @@
+/**
+Cookie Calories
+A bag of cookies holds 40 cookies. 
+The calorie information on the bag claims that there are 
+10 servings in the bag and that serving equals 300 calories.
+Write a program that lets the user enter the number of cookies he
+or she actually ate and then reports the number of total calories consumed.
+*/
+
+
+
 import javax.swing.JOptionPane;
 
 public class CookieCalories
@@ -20,7 +31,7 @@ public class CookieCalories
    cookiesAte = Integer.parseInt(cookiesAteStr);
    
    //output
-   System.out.println("You've consumed " +(cookiesAte * oneCookie)+" calories.");
+   System.out.println("You've consumed " +(cookiesAte * oneCookie)+ " calories.");
    
    System.exit(0);
    
